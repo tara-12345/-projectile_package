8903c928cb8fb70c6dea2c27818d5a12ce6501db
Shaamini Prinja <shaaminiprinja@Shaaminis-MacBook-Air>
2023-11-27 16:47:15 +0000

    added files

--- projectile_package/.ipynb_checkpoints/projectile-checkpoint.py
+++ projectile_package/.ipynb_checkpoints/projectile-checkpoint.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from numpy import sign
+
 def update_state(t, x, v, a, dt=0.1):
     distance_moved = v*dt + (1/2)*a*(dt**2)
     v += a*dt
@@ -10,57 +13,59 @@ def update_state(t, x, v, a, dt=0.1):
 def calculate_acceleration_y(v, k=0.0, mass=1.0, gravity=-9.81):
     force_gravity = mass*gravity
     force_air = -sign(v)*k*v**2
-    total_force = force_air
+    total_force = force_air+force_gravity
     a = total_force/mass
     
     return a
 
-def calculate_acceleration_x(v, k=0.0, mass=1.0, gravity=-9.81):
-    force_gravity = mass*gravity
+def calculate_acceleration_x(v, k=0.0, mass=1.0):
+    
     force_air = -sign(v)*k*v**2
-    total_force = force_gravity + force_air
-    a = total_force/mass
+    a = force_air/mass
     
     return a
 
 
-def flying_mass(v, vx, k=0.0, mass=1.0, dt=0.1):
-    start_velocity = 0.0 # m/s
+def flying_mass(vy, vx, k=0.0, mass=1.0, dt=0.1):
     gravity = -9.81 # m/s2
+    initial_height = 0.0
 
     # Initial values for our parameters
     distance_moved = 0
     y = initial_height
-    v = start_velocity
+    vy = vy
     t = 0.0
     x = 0.0
     a_x = 0.0
     a_y = 0.0
-    vx = start_velocity
+    vx = vx
     
     # Create empty lists which we will update
     height = []
     velocity = []
     time = []
     x_coords = []
-    
+    velocity_x = []
+
       # Keep looping while the object is still falling
-    while h > 0:
-        a_x = calculate_acceleration_x(v, k=k, mass=mass, gravity=gravity)
-        a_y = calculate_acceleration_y(v, k=k, mass=mass, gravity=gravity)
+    while y >= 0:
+        a_x = calculate_acceleration_x(vx, k=k, mass=mass)
+        a_y = calculate_acceleration_y(vy, k=k, mass=mass, gravity=gravity)
 
         # Append values to list and then update
         height.append(y)
-        velocity.append(v)
+        velocity.append(vy)
         time.append(t)
         x_coords.append(x)
+        velocity_x.append(vx)
         
+
         # Update the state for time, height and velocity
-        t, y, v = update_state(t, h, v, a, dt=dt)
-        t, x, vx = update_state(t, h, v, a, dt=dt)
+        t, y, vy = update_state(t, y, vy, a_y, dt=dt)
+        t, x, vx = update_state(t, x, vx, a_x, dt=dt)
         
     
-    return t, x, y, vx, v
+    return time, x_coords, height, velocity_x, velocity
 
    
     
